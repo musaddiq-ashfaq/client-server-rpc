bc5e3a7e0386e30b0cbb682cca0a8ff55402ffc9
moaz farrukh <maazfarrukh@gmail.com>
2025-02-13 12:46:06 +0500

    feat: randomly assign ports to workers

--- worker/main.go
+++ worker/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"net/rpc"
 	"time"
 
-	worker "worker/matrix_operation"
 	client "client/matrix_request"
+	worker "worker/matrix_operation"
 )
 
 // WorkerService defines an RPC service for matrix computations.
@@ -18,7 +17,7 @@ type WorkerService struct {
 
 // Compute handles matrix operation requests from the Coordinator.
 func (w *WorkerService) Compute(req client.MatrixRequest, res *client.MatrixResponse) error {
-	
+
 	result, err := worker.PerformMatrixOperation(req)
 	if err != nil {
 		return err
@@ -52,20 +51,23 @@ func registerWithCoordinator(workerAddr string) {
 }
 
 func main() {
-	workerPort := 50052 // Worker starts on this port
-	workerAddr := fmt.Sprintf("localhost:%d", workerPort)
+
+	// Start listening for Coordinator requests
+	workerHost := "localhost"
+	
+	listener, err := net.Listen("tcp", workerHost+":0")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	workerAddr := listener.Addr().String()
 
 	// Register the RPC service
 	workerService := &WorkerService{Address: workerAddr}
-	err := rpc.Register(workerService)
+
+	err = rpc.Register(workerService)
 	if err != nil {
 		log.Fatalf("Error registering worker RPC: %v", err)
-	}
-
-	// Start listening for Coordinator requests
-	listener, err := net.Listen("tcp", workerAddr)
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
 	}
 
 	log.Printf("Worker is running on %s...", workerAddr)
